e0a7759f7e219bdcea1fe5c45958e5b80ad6eff7
Chetanya Hindwan <chetanyaintense77@gmail.com>
2026-09-07 19:28:08 +0530

    Signed-off-by: Chetanya Hindwan <chetanyaintense77@gmail.com>

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const { startDigestCron } = require("./services/digestCron");
 const digestRoute = require("./routes/digestRoute")
 
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: process.env.FRONTEND_URL,
   credentials: true
 }));
 
